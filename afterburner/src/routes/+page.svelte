468ca0d90424e886dc92a96d924da1c0707184b2
Celestine Simone Machuca Iturriaga <celestine.s.machuca@gmail.com>
2025-06-27 13:49:57 +0200

    d

--- afterburner/src/routes/+page.svelte
+++ afterburner/src/routes/+page.svelte
@@ -2,19 +2,7 @@
     import Chart from "chart.js/auto";
     import { SerialPort } from "tauri-plugin-serialplugin";
     import { onMount } from "svelte";
-    let canvas: HTMLCanvasElement;
-    let ctx;
 
-    let chartValues = [20, 10, 5, 2, 20, 30, 45];
-    let chartLabels = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-    ];
     onMount(async () => {
 
 
@@ -25,11 +13,4 @@
     
 </script>
 
-<canvas bind:this={canvas}></canvas>
-
-<div class="grid grid-cols-4 gap-4">
-    <div>01</div>
-    <!-- ... -->
-    <div>09</div>
-</div>
 <button class="btn">Connect to Serial</button>
