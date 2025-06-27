1009822c633c576c6efea5b79bc821b7c1ac2cdf
Celestine Simone Machuca Iturriaga <celestine.s.machuca@gmail.com>
2025-06-27 15:29:34 +0200

    eded

--- afterburner/src/routes/+page.svelte
+++ afterburner/src/routes/+page.svelte
@@ -2,15 +2,23 @@
     import Chart from "chart.js/auto";
     import { SerialPort } from "tauri-plugin-serialplugin";
     import { onMount } from "svelte";
-
+    let available_ports: string[] = [];
     onMount(async () => {
 
 
     // List available ports
     const ports = await SerialPort.available_ports();
-    console.log("Available ports:", ports);
+    console.log(ports[0].path);
+
     });
     
 </script>
 
-<button class="btn">Connect to Serial</button>
+<details class="dropdown">
+  <summary class="btn m-1">Connect</summary>
+  <ul class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    {#each available_ports as port}
+        <li><a>{port}</a></li>
+    {/each}
+  </ul>
+</details>
