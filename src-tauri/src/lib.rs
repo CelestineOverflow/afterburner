20a68e248f5068758bd612139007192677d6db7b
Celestine Simone Machuca Iturriaga <celestine.s.machuca@gmail.com>
2026-02-06 10:55:40 +0100

    added web server

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,27 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
